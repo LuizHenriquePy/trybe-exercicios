f614bdfc07bb9335663b1a788a801d11231289b2
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-05-23 17:23:34 -0300

    segunda parte do exercicio feita

--- fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ex_02/scripts.js
+++ fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ex_02/scripts.js
@@ -13,23 +13,38 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+// Add days
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-const daysList = document.getElementById('days');
-for (let item of dezDaysList) {
-  let day = document.createElement('li');
-  let listC = day.classList;
-  listC.add('day');
-  day.innerText = item;
+function addDays () {
+  const daysList = document.getElementById('days');
+  for (let item of dezDaysList) {
+    let day = document.createElement('li');
+    let listC = day.classList;
+    listC.add('day');
+    day.innerText = item;
 
-  if (item === 24 || item === 25 || item === 31) {
-    listC.add('holiday');
+    if (item === 24 || item === 25 || item === 31) {
+      listC.add('holiday');
+    };
+    if (item === 4 || item === 11 || item === 18 || item === 25) {
+      listC.add('friday')
+    };
+
+    daysList.appendChild(day);
+
   };
-  if (item === 4 || item === 11 || item === 18 || item === 25) {
-    listC.add('friday')
-  };
-
-  daysList.appendChild(day);
-
 };
+
+addDays();
+
+// Add button "Feriados"
+function addHolidayButton (text) {
+  const element = document.querySelector('.buttons-container');
+  let button = document.createElement('button');
+  button.id = 'btn-holiday';
+  button.textContent = text;
+  element.appendChild(button);
+};
+
+addHolidayButton('Feriados');
