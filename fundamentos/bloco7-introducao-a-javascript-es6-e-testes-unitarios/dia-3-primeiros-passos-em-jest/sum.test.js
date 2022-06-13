77e2668e2cc4506c106f51e295fd6b18b33e731d
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-06-13 15:06:52 -0300

    funcao sum testada

--- fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,7 +1,16 @@
 const sum = require('./sum.js');
 
-describe("Testa função sum", () => {
-  it("sum(4,5) retorna 9", () => {
+describe('Testa função sum', () => {
+  it('sum(4,5) retorna 9', () => {
     expect(sum(4,5)).toBe(9);
+  });
+  it('sum(0,0) retorna 0', () => {
+    expect(sum(0,0)).toBe(0);
+  });
+  it('sum(4,"5") retorna erro', () => {
+    expect(() => sum(4,"5")).toThrow(Error);
+  });
+  it('sum(4,"5") retorna erro com mensagem "parameters must be numbers"', () => {
+    expect(() => sum(4,"5")).toThrow('parameters must be numbers');
   });
 });
