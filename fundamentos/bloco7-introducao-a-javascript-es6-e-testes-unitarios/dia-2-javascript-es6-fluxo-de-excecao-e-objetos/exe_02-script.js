d8b7a058347d2c0caeee47fc894e0f7e689c0b1a
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-06-14 16:43:10 -0300

    parte 2 do exercicio do dia

---
+++ fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exe_02-script.js
@@ -0,0 +1,44 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+// "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701"
+const customerInfo = (order) => {
+  const values = Object.values(order);
+  const address = Object.values(values[2]);
+  return `Olá ${values[0]}, entrega para: ${order['order']['delivery']['deliveryPerson']}, Telefone: ${values[1]}, ${address[0]}, Nº: ${address[1]}, AP: ${address[2]}`;
+}
+
+console.log(customerInfo(order));
