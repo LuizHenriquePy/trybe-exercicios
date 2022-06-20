eb13d16e94707d7324bad2762be76d52f0e148e9
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-06-20 10:35:57 -0300

    Primeiro projeto em react

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/study-app/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component {
+  render () {
+    return(
+      <h1>Conteúdos do bloco Front-end</h1>
+    );
+  }
+}
+
+export default Header;
